f16cb06d10b66e14789940717dfb9b6e14b53ce8
shad0wrider <githubpush@outlook.com>
2025-05-25 14:46:25 +0530

    Update saltyv2.py

--- src/saltyv2.py
+++ src/saltyv2.py
@@ -24,9 +24,8 @@ symnumbind = {0:'&#',1: '`', 2: '~', 3: '!', 4: '@', 5: '#', 6: '$', 7: '%', 8: 
 
 digitbind = {0:'zx',1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j',11:'k',12:'l',13:'m',14:'n',15:'o',16:'p',17:'q',18:'r',19:'s',20:'t',21:'u',22:'v',23:'w',24:'x',25:'y',26:'z'}
 
-jumbpass = []
-
 def passmixer(password:str):
+  jumbpass = []
   passw = password.lower()
     
   if len(passw) % 2 ==0:
